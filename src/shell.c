3ea47456336f2f20aa5abb6d184aae2e5a272ba1
Leland <lelandmiller@gmail.com>
2014-04-09 06:59:05 +0000

    Added ability to fork and execute external command.

--- src/shell.c
+++ src/shell.c
@@ -2,19 +2,45 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <sys/wait.h>
 
 extern char **get_line(void);
 
-int main(int argc, char *argv[]) {
-  int i;
-  char **args; 
+int run_external(char **args) {
+  pid_t pid;
+  pid = fork();
+  if (pid > 0) {
+    /* Then we are parent */
+    int status;
+    wait(&status);
+  } else if (pid == 0) {
+    /* Then we are child */
+    char *p = "";
+    execvp(args[0], args);
+  } else {
+    /* TODO Error */
+  }
+}
 
+void print_chrome() {
+  printf(">> ");
+}
+
+int main(int argc, char *argv[]) {
+  int i; 
+  char **args; 
   while(1) {
+    print_chrome();
     args = get_line();
     if(!args) break;
     for(i = 0; args[i] != NULL; i++) {
       printf("Argument %d: %s\n", i, args[i]);
     }
+    if (strcmp(args[0],"exit") == 0) {
+      exit(0);
+    }
+    run_external(args);
   }
   return 0;
 }
+
