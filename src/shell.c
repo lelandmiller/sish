11b910fae3d38bed08dff44e9a42a669e537afae
Leland <lelandmiller@gmail.com>
2014-04-15 02:38:54 +0000

    Started implementing redirection and option parsing. Fixed warning about old-fashioned function declaration.

--- src/shell.c
+++ src/shell.c
@@ -3,11 +3,12 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <stdbool.h>
 
 extern char **get_line(void);
 
 
-int run_external(char **args) {
+int run_external(char **args, char* input, char* output,  bool bg) {
   pid_t pid;
   pid = fork();
   if (pid > 0) {
@@ -15,7 +16,15 @@ int run_external(char **args) {
     int status;
     wait(&status);
   } else if (pid == 0) {
+    
     /* Then we are child */
+    if (input) {
+      freopen(input, "r", "stdin");
+    }
+    if (output) {
+
+    }
+
     execvp(args[0], args);
     /* At this point there was an error in execvp */
     fprintf(stderr, "Error opening: %s.", args[0]);
@@ -24,22 +33,33 @@ int run_external(char **args) {
   }
 }
 
-void print_chrome() {
+void print_chrome(void) {
   /* switch color */
   printf("\x1b[35;1m");
   printf(">> ");
   printf("\x1b[0m");
 }
 
+int count_array(char **args) {
+  int c = 0;
+  while (*args != NULL) {
+    c++;
+    args++;
+  }
+  return c;
+}
+
 void run_command(char **args) {
+  /*bool bg = false;*/
   char *cmd = args[0];
+  printf("argc: %d\n", count_array(args));
   if (strcmp(cmd,"exit") == 0) {
     exit(0);
   } else if (strcmp(cmd,"cd") == 0) {
     /* TODO needs error checking on input and chdir return */
     chdir(args[1]);
   } else {
-    run_external(args);
+    run_external(args, "test.txt", NULL, false);
   }
 }
 
@@ -53,6 +73,7 @@ int main(int argc, char *argv[]) {
     for(i = 0; args[i] != NULL; i++) {
       printf("Argument %d: %s\n", i, args[i]);
     }
+
     run_command(args);
   }
   return 0;
