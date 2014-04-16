ec938905b7e76b6866b8f5c111539de59e2bcd5c
Leland Miller <lelandmiller@gmail.com>
2014-04-16 18:59:45 +0000

    Added error checking and comments, ready to turn in.

--- src/shell.c
+++ src/shell.c
@@ -1,3 +1,7 @@
+/* shell.c
+ * Contains the main code for shell functionality.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -7,20 +11,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* MAX_PATH_LENGTH is the longest path name supported (used for print_chrome)
+ * MAX_COMMANDS is the longest number of arguments allowed for one command
+ */
 #define MAX_PATH_LENGTH 256
 #define MAX_COMMANDS 256
 
 extern char **get_line(void);
 
-
+/* This function forks and executes a new process, input is a possible input
+ * redirection (NULL is none), output is a possible output redirection,
+ * and bg is true for a background process. 
+ */
 int run_external(char **args, char* input, char* output,  bool bg) {
-  pid_t pid;
+  pid_t pid; /* Will be the pid of the fork result */
   pid = fork();
   if (pid > 0) {
     /* Then we are parent */
     int status;
     pid_t reaped_pid;
     if (!bg) { 
+      /* This is the loop to reap zombies */
       do {
         reaped_pid = wait(&status);
         if (reaped_pid != pid) {
@@ -40,30 +51,62 @@ int run_external(char **args, char* input, char* output,  bool bg) {
     }
 
     execvp(args[0], args);
+
     /* At this point there was an error in execvp */
     fprintf(stderr, "Error opening: %s.", args[0]);
+    
   } else {
-    /* TODO Error */
+    fprintf(stderr, "Error forking a child process");
   }
 }
 
+/* Prints the prompt with current directory and colors */
 void print_chrome(void) {
   /* switch color */
   char cwd[MAX_PATH_LENGTH];
-  getcwd(cwd,MAX_PATH_LENGTH);
-  printf("\x1b[35;1m");
-  printf("%s \x1b[32;1m>> ", cwd);
-  printf("\x1b[0m");
+  if (getcwd(cwd,MAX_PATH_LENGTH)) {
+    printf("\x1b[35;1m");
+    printf("%s \x1b[32;1m>> ", cwd);
+    printf("\x1b[0m");
+  } else {
+    printf("\x1b[32;1m>> \x1b[0m");
+  }
 }
 
+/* Takes a list of arguments an changes the working directory to
+ * argument 1, checking for errors 
+ */
+change_dir(char **args) {
+  if (args[1] == NULL) {
+    fprintf(stderr, "Usage: cd path\ncd requires a path\n");
+  } else {
+    if (chdir(args[1]) < 0) {
+      fprintf(stderr, "Cannot cd into %s\n", args[1]);
+    }
+  }
+}
+
+/* Takes a list of arguments, gets all the flags and shell
+ * commands, and if required sends an array of the rest of
+ * the arguments to run_external()
+ */
 void run_command(char **args) {
-  char *new_args[MAX_COMMANDS];
-  char *cmd;
-  char *input = NULL;
-  char *output = NULL;
-  bool bg = false;
+  char *new_args[MAX_COMMANDS]; /* Holds filtered arguments */
+  char **new_args_i = new_args; /* Used as a pointer in loop */
   
-  char **new_args_i = new_args;
+  char *cmd; /* Will be pointer to the first argument */
+
+  /* "Flags" for shell commands */
+  char *input = NULL;  /* Input redirection, NULL if none */
+  char *output = NULL; /* Output redirrection, NULL is none */
+  bool bg = false;     /* Should this command run in the background */
+
+  /* Check for an empty command */
+  if (*args == NULL) {
+    return;
+  }
+
+  /* Loop through all arguments */
   while (*args != NULL) {
     if (strcmp(*args,"<") == 0) {
       args++;
@@ -89,25 +132,27 @@ void run_command(char **args) {
   }
   *new_args_i = NULL;
 
+  /* Decide what to do */
   cmd = new_args[0];
   if (strcmp(cmd,"exit") == 0) {
     exit(0);
   } else if (strcmp(cmd,"cd") == 0) {
-    /* TODO needs error checking on input and chdir return */
-    chdir(new_args[1]);
+    change_dir(new_args);
   } else {
     run_external(new_args, input, output, bg);
   }
 }
 
+/* Entry point, simple input loop */
 int main(int argc, char *argv[]) {
   int i; 
   char **args; 
   while(1) {
     print_chrome();
     args = get_line();
+    /* Checks end of file */
     if(!args) break;
-    /*
+    /* Used for debugging
     for(i = 0; args[i] != NULL; i++) {
       printf("Argument %d: %s\n", i, args[i]);
     }
